45bbb540c4a47e249bad564615424cae06f164b9
nicolozibra1 <nico.zibra@gmail.com>
2023-03-08 16:03:17 +0100

    create-variable

--- js/script.js
+++ js/script.js
@@ -4,3 +4,46 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+// {MY SCRIPT}
+
+// creo due variabili che conterranno i dati in input del utente. (Valore di ritorno di un prompt)
+let mileage = prompt("Inserisci la quantità di km che vuoi percorrere");
+let age = prompt("Inserisci la tua età");
+
+console.log(mileage + age);
+
+// creo una constante contenente il prezzo fisso per km
+const priceForKm = 0.21;
+
+console.log(priceForKm);
+// calcolo un costo parziale sulla base di km da percorrere e prezzo per km
+let partialPrice = mileage * priceForKm;
+
+console.log(partialPrice);
+
+// creo due constanti contenenti i possibili sconti che verranno applicati sul prezzo del biglietto
+const discount20 = 0.20;
+const discount40 = 0.40;
+
+//creo due variabili che calcoleranno lo sconto da applicare sul prezzo finale
+let discountApplied20 = partialPrice * discount20;
+let discountApplied40 = partialPrice * discount40;
+
+console.log(discountApplied20);
+
+// creo più condizioni che mi permettano di verificare -se e quale- sconto applicare al utente, sulla base delle informazioni che utente mi ha fornito
+if (age < 18) {
+    partialPrice -= discountApplied20;
+}
+if (age > 64) {
+
+}
+else{
+
+}
+
+
+
+console.log();
